c0bf3dd3b3bc24e1f0ddaf1d194a5ef30d88efc1
Piotr Baran <xpiterros@gmail.com>
2022-05-22 19:19:15 +0200

    LAB 2

--- src/main/java/com/example/lab2javafx/HelloController.java
+++ src/main/java/com/example/lab2javafx/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.lab2javafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
+    int clickCounter = 0;
+    public Button btnMain;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    protected void onBtnAction() {
+        System.out.println("Kliknięcie nr" + ++clickCounter);
+        if (clickCounter ==1 )
+            btnMain.setText("I jeszcze raz");
     }
 }
